a94bf98a5fa1bf59626822ce023128d7393910bd
Mohammed Azim Moula <azimmoula1@gmail.com>
2025-07-21 14:12:54 +0530

    Added Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:kindmap/config/routes.dart';
+import 'package:kindmap/firebase_options.dart';
 import 'package:kindmap/screens/homescreen.dart';
-
 import 'config/app_theme.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
