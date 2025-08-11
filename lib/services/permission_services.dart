2f60e247d578815d7935fa2fc331e30d5bab3203
Abdul Rafey Waleed <131298842+rafeywaleed@users.noreply.github.com>
2025-08-11 13:39:05 +0530

    Create permission_services.dart

---
+++ lib/services/permission_services.dart
@@ -0,0 +1,34 @@
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class PermissionService {
+  static Future<bool> handleLocationPermission() async {
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return false;
+    }
+
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return false;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return false;
+    }
+
+    return true;
+  }
+
+  static Future<bool> handleNotificationPermission() async {
+    final status = await Permission.notification.status;
+    if (status.isDenied) {
+      final result = await Permission.notification.request();
+      return result.isGranted;
+    }
+    return status.isGranted;
+  }
+}
